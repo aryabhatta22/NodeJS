188df111cbf5b53b6d132707a28ab36b0881d250
aryabhatta <tarun.hive@hotmail.com>
2019-10-12 15:19:00 +0530

    replace module module created

--- Begin/script.js
+++ Begin/script.js
@@ -58,20 +58,7 @@
 const http = require('http');
 const url = require('url');
 const fs = require('fs');
-
-const replaceTemplate = ( temp, product ) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g , product.productName); // /{%PRODUCTNAME%}/g is regular expression to replace all placeholder not just the first one
-  output = output.replace(/{%IMAGE%}/g , product.image);
-  output = output.replace(/{%PRICE%}/g , product.price);
-  output = output.replace(/{%FROM%}/g , product.from);
-  output = output.replace(/{%NUTRIENTS%}/g , product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g , product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g , product.description);
-  output = output.replace(/{%ID%}/g , product.id);
-
-  if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g ,  'not-organic');
-  return output;
-}
+const replaceTemplate = require('./modules/replaceTemplate');
 
 //this is the top level code and needs to be called only once hence blocking code style is used
 const Overview = fs.readFileSync(`${__dirname}/Overview.html`, 'utf-8');
