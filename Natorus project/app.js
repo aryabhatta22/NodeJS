999b12273cd81ff2ac3efca146af143df747422b
aryabhatta <tarun.hive@hotmail.com>
2020-03-05 12:34:17 +0530

    tours API created

--- Natorus project/app.js
+++ Natorus project/app.js
@@ -1,21 +1,46 @@
+const fs = require('fs');
 const express = require('express');
 
+
 const app = express();
+app.use(express.json());
 const port = 3000;
 
-app.get('/', (req, res) => {
-  // res.status(200).send('Hello from the server');
-  //or
 
+const toursData = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
-    message: 'Hello from the server side',
-    app: 'Natours'
-  });
-});
+    status: 'success',
+    results: toursData.length,
+    data: {
+      tours: toursData
+    }
+  })
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint');
-});
+})
+
+app.post('/api/v1/tours', (req,res) => {
+  
+  const newId = toursData[toursData.length -1].id+1;
+  const newTour = Object.assign({id: newId}, req.body);
+
+  toursData.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(toursData),
+    err => {
+    res.send(201).json({
+      status: 'success',
+      data: {
+        tour: newTour
+      }
+    });
+  }
+  );
+
+})
 
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
