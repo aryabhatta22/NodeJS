12c35d264b3b054e0b886e6166fd223fabc2750d
aryabhatta <tarun.hive@hotmail.com>
2020-09-09 19:25:48 +0530

    added: Filtering and sorting query

--- Natorus project/controllers/tourController.js
+++ Natorus project/controllers/tourController.js
@@ -5,8 +5,44 @@ const Tour = require('./../models/tourModels')
 exports.getAllTours = async (req, res) => {
 
   try {
-    const tours = await Tour.find()
 
+    console.log('URL query: \n', req.query)
+    // BUILD QUERY
+        // 1) Filtering
+
+    const queryObj = {...req.query}   // shallow copy of  query object
+    const excludedFields = ['page', 'sort','limit', 'fields']
+    excludedFields.forEach(el => delete queryObj[el])
+
+        // 1) Advance Filtering
+
+    let queryStr = JSON.stringify(queryObj)
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+    console.log('After excluding: \n',JSON.parse(queryStr))
+
+    let query = Tour.find(JSON.parse(queryStr))
+
+            // 3) Sorting
+
+            /* we need to split the sorting fields by "," from the URL query string
+              Then we will seperate each field by space and join them as
+              .../tours?sort=price,rating     --->  sort(price rating)   for mongoose query   */
+
+    if(req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ')
+      console.log('Sorting Fields: \n',sortBy)
+      query = query.sort(sortBy)
+    } else {
+      // Deafult sorting field
+      query = query.sort('-createdAt')
+    }
+
+
+    // EXECUTE QUERY
+    const tours = await query
+
+
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       results: tours.length,
@@ -86,19 +122,19 @@ exports.UpdateTour = async (req, res) => {
   }
 }
   
-  exports.deleteTour = async (req, res) => {
-    try {
-      const tour = await Tour.findByIdAndDelete(req.params.id);
+exports.deleteTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndDelete(req.params.id);
 
-      res.status(204).json({
-        status: 'success',
-        data: null
-      })
-    }catch(err) {
-      res.status(404).json({
-        status: 'fail',
-        message: err
-      })
-    }
+    res.status(204).json({
+      status: 'success',
+      data: null
+    })
+  }catch(err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    })
   }
+}
   
