15fce31c6c2fd97fff1b70f142fd41248b599827
aryabhatta <tarun.hive@hotmail.com>
2020-09-11 21:03:49 +0530

    added: aggregation mehod: unwind

--- Natorus project/routes/tourRoutes.js
+++ Natorus project/routes/tourRoutes.js
@@ -14,6 +14,10 @@ router
   .route('/tour-stats')
   .get(tourController.getTourStats)
 
+  router
+  .route('/monthly-plan/:year')
+  .get(tourController.getMonthlyPlan)
+
 router
   .route('/')
   .get(tourController.getAllTours)
