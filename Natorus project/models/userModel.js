e6901d71a5b80e79bd1e05c992133ec67acaada9
aryabhatta <tarun.hive@hotmail.com>
2020-09-14 17:23:47 +0530

    added: user model schema

---
+++ Natorus project/models/userModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const validator = require('validator')
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please tell us you name']
+    },
+    email: {
+        type: String,
+        required: [true, 'Please tell us your email'],
+        unique: true,
+        lowercase: true,
+        validate: [validator.isEmail, 'Please provide a valid email']
+    },
+    photo: String,
+    password: {
+        type: String,
+        required: [true, 'Provide a password'],
+        minlength: 8,
+    },
+    passwordConfirm: {
+        type: String,
+        required: [true, 'Please confirm the password.']
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
