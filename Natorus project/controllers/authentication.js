bcafbc191c38f489b25308fa29f6be9eb5b1e521
aryabhatta <tarun.hive@hotmail.com>
2020-09-21 17:16:41 +0530

    added: nodemailer to send email

--- Natorus project/controllers/authentication.js
+++ Natorus project/controllers/authentication.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/userModel')
 const catchAsync = require('../utils/catchAsync')
 const AppError = require('../utils/appError')
+const sendEmail = require('../utils/email')
 
 const signToken = id => {
     return jwt.sign({id}, process.env.JWT_SECRET, {
@@ -82,4 +83,46 @@ exports.restrictTo = (... roles) => {
             return next(new AppError('You do not have permission to perform this action', 403))
         next();
     }
+}
+
+exports.forgotPassword = catchAsync( async (req,res,next) => {
+
+    // 1. Get user based on Posted email
+    const user = await User.findOne({email: req.body.email})
+    if(!user) {
+        return next(new AppError('Theres no user with this email', 404))
+    }
+
+    // 2. Generate the random reset token
+    const resetToken = user.changePasswordResetToken();
+    console.log({resetToken})
+    await user.save({validateBeforeSave: false  });
+
+    //3. send it to user's email
+    const resetURL = `${req.protocol}://${req.get('host')}/api/v1/users/resetPassword/${resetToken}`
+    const message = `Forgot your password? Submit a patch request with you new password and passwordConfirm to: ${resetURL}.
+                    If you didn't forgot your password, please ignore this email!`;
+    try {
+        await sendEmail({
+            email: user.email,
+            subject: 'Your Password reset token(valid for 10 min)',
+            message
+        });
+    
+        res.status(200).json({
+            status: 'success',
+            message: 'Token sent to email!'
+        })
+    } catch(err) {
+        user.changePasswordResetToken = undefined;
+        user.passwordResetExpires = undefined;
+
+        await user.save({validateBeforeSave: false  });
+        return next(new AppError('There was an error sending the email. Try again.', 500));
+
+    }
+});
+
+exports.resetPassword = (req,res,next) => {
+    
 }
