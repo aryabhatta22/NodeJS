5051296990685e1a6d315ccd0e98c5d6d7705749
aryabhatta <tarun.hive@hotmail.com>
2020-09-24 13:08:37 +0530

    added: additional security methods

--- Natorus project/app.js
+++ Natorus project/app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet')
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean')
+const hpp = require('hpp')
 
 const AppError = require('./utils/appError')
 const globalErrorHandler =  require('./controllers/errorController')
@@ -10,14 +15,49 @@ const app = express();
 
 
 
-/* ----------------------- Middlewares -----------------------*/
+/* ----------------------- Gloabl Middlewares -----------------------*/
+// Security HTTP headers
+app.use(helmet());
+
+// Developmetn logging
 console.log(`In ${process.env.NODE_ENV} mode`);
 if(process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(express.json());
 
+// Limit requests from same API
+const limiter = rateLimit({
+  // 100 req in an hour
+  max: 100,
+  windowMs: 60 * 60 * 100,
+  message: 'Too many request from this IP, please try again in an hour'
+});
+
+app.use('/api',limiter);
+
+// Body parser, reading data from body into req.body
+app.use(express.json({limit: '10kb'}));
+
+
+// Data sanitiazation against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+//Prevent parameter pollution
+app.use(hpp({
+  whitelist: [
+    'duration','ratingsAverage','maxGroupSize',
+    'ratingsQuantity', 'difficulty', 'price'
+  ]
+}));
+
+// serving static files
+app.use(express.static(`${__dirname}/public`))
+
+// Test middleware
 app.use((req, res, next)=> {
   req.reuestTime = new Date().toISOString();
   // console.log(req.headers)
